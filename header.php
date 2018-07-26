589e5c20969d43feaf15f0b8abcdadefa85b6f6f
Sam Gates <sam@covertnine.com>
2018-07-26 14:14:20 -0500

    more visual tweaks

--- header.php
+++ header.php
@@ -12,16 +12,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <script>
-  (function(d) {
-    var config = {
-      kitId: 'syz4pwm',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
-</script>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -7,23 +7,19 @@
 
 ?>
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+  
+      <div class="entry-meta">
 
+      <?php understrap_posted_on(); ?>
+
+    </div><!-- .entry-meta -->
 	<header class="entry-header">
-
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-		<div class="entry-meta">
-
-			<?php understrap_posted_on(); ?>
-
-		</div><!-- .entry-meta -->
-
 	</header><!-- .entry-header -->
-  
-  <div class="featured-image">
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+       <div class="featured-image">
+  <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
   </div>
-
 	<div class="entry-content">
 
 		<?php the_content(); ?>
